21259defb06447919c9261da242e82d7d25a765f
vod <dannyphvo@gmail.com>
2020-12-23 18:56:40 -0600

    added rw mutex

--- pkg/fibonacci/fibonacci.go
+++ pkg/fibonacci/fibonacci.go
@@ -1,9 +1,12 @@
 package fibonacci
 
+import "sync"
+
 type Fibonacci struct {
 	current  uint32
 	next     uint32
 	previous uint32
+	rw_mutex *sync.RWMutex
 }
 
 func Initialize_Fibonacci() *Fibonacci {
@@ -15,16 +18,26 @@ func Initialize_Fibonacci() *Fibonacci {
 }
 
 func (f *Fibonacci) Get_Current() uint32 {
+	f.rw_mutex.RLock()
+	defer f.rw_mutex.RUnlock()
+
 	return f.current
 }
 
 func (f *Fibonacci) Get_Next() uint32 {
+	f.rw_mutex.Lock()
+	defer f.rw_mutex.Unlock()
+
 	f.previous = f.current
 	f.current = f.next
 	f.next = f.current + f.previous
+
 	return f.next
 }
 
 func (f *Fibonacci) Get_Previous() uint32 {
+	f.rw_mutex.RLock()
+	defer f.rw_mutex.RUnlock()
+
 	return f.previous
 }
