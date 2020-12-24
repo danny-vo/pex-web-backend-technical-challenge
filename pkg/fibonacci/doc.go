91ef912a4f42a6bf36a232f32824ba93dc97fa02
vod <dannyphvo@gmail.com>
2020-12-24 04:54:00 -0600

    added go docs

---
+++ pkg/fibonacci/doc.go
@@ -0,0 +1,2 @@
+// Package fibonacci contains a wrapper object for the Fibonacci sequence
+package fibonacci // import "github.com/danny-vo/fibonacci-backend/pkg/fibonacci"
---
+++ pkg/server/doc.go
@@ -0,0 +1,3 @@
+// Package server contains a wrapper object for the primary server including
+// routes and handler functions
+package server // import "github.com/danny-vo/fibonacci-backend/pkg/server"
