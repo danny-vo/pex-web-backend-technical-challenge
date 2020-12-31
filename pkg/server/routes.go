e29b7cafacd2cd56416059bc88b045cf47d994ec
danny-vo <dannyphvo@gmail.com>
2020-12-30 22:06:40 -0600

    remove trailing slashes, added cpu limit (unix), swap limit

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -5,8 +5,8 @@ import "net/http"
 // This funciton initializes all the methods, routes, and assigns handlers for
 // the server's router
 func (s *Server) routes() {
-	s.router.HandlerFunc(http.MethodGet, "/current/", s.handle_current())
-	s.router.HandlerFunc(http.MethodGet, "/next/", s.handle_next())
-	s.router.HandlerFunc(http.MethodGet, "/previous/", s.handle_previous())
-	s.router.HandlerFunc(http.MethodGet, "/health/", s.handle_health())
+	s.router.HandlerFunc(http.MethodGet, "/current", s.handle_current())
+	s.router.HandlerFunc(http.MethodGet, "/next", s.handle_next())
+	s.router.HandlerFunc(http.MethodGet, "/previous", s.handle_previous())
+	s.router.HandlerFunc(http.MethodGet, "/health", s.handle_health())
 }
