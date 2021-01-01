c4156757d876f65f095e82710950169700acc400
danny-vo <dannyphvo@gmail.com>
2021-01-01 01:18:59 -0600

    minor refactor to test handlers better

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -9,38 +9,32 @@ import (
 // handleCurrent -
 // This function should return the current number in the Fibonacci sequence.
 func (s *Server) handleCurrent() http.HandlerFunc {
-	return recoveryWrapper(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(fmt.Sprintf(`{"current": %d}`, s.fibSequence.GetCurrent())))
-		},
-	)
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`{"current": %d}`, s.fibSequence.GetCurrent())))
+	}
 }
 
 // handleNext -
 // This function should return the next number in the Fibonacci sequence and
 // progress the series.
 func (s *Server) handleNext() http.HandlerFunc {
-	return recoveryWrapper(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(fmt.Sprintf(`{"next": %d}`, s.fibSequence.GetNext(s.rdb))))
-		},
-	)
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`{"next": %d}`, s.fibSequence.GetNext(s.rdb))))
+	}
 }
 
 // handlePrevious -
 // This function returns the previous number in the Fibonacci sequence.
 func (s *Server) handlePrevious() http.HandlerFunc {
-	return recoveryWrapper(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(fmt.Sprintf(`{"previous": %d}`, s.fibSequence.GetPrevious())))
-		},
-	)
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`{"previous": %d}`, s.fibSequence.GetPrevious())))
+	}
 }
 
 // handleHealth -
--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -5,8 +5,8 @@ import "net/http"
 // This funciton initializes all the methods, routes, and assigns handlers for
 // the server's router
 func (s *Server) routes() {
-	s.router.HandlerFunc(http.MethodGet, "/current", s.handleCurrent())
-	s.router.HandlerFunc(http.MethodGet, "/next", s.handleNext())
-	s.router.HandlerFunc(http.MethodGet, "/previous", s.handlePrevious())
+	s.router.HandlerFunc(http.MethodGet, "/current", recoveryWrapper(s.handleCurrent()))
+	s.router.HandlerFunc(http.MethodGet, "/next", recoveryWrapper(s.handleNext()))
+	s.router.HandlerFunc(http.MethodGet, "/previous", recoveryWrapper(s.handlePrevious()))
 	s.router.HandlerFunc(http.MethodGet, "/health", s.handleHealth())
 }
